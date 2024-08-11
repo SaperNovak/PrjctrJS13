eed517264121755e0df947262211c3c6266b7361
SaperNovak <98357434+SaperNovak@users.noreply.github.com>
2024-08-11 21:35:13 +0300

    Upload to correct branch

--- app.js
+++ app.js
@@ -5,22 +5,49 @@
 але замість чисел кратних 3 має виводитись `Лол`, замість значень кратних 5 - `Кек`, 
 а замість значень кратних і 3, і 5 - `ЛолКек
 */
-const RandVal = Math.trunc(Math.random() * 100);
+//const RandVal = Math.trunc(Math.random() * 100);
 //console.log(RandVal); 
 
-switch (true) {
-    case !!!(RandVal % 15):
-        console.log("ЛолКек");
-    break;
-    case !!!(RandVal % 3):
-        console.log("Лол");
-    break;
-    case !!!(RandVal % 5):
-        console.log("Кек");
-    break;
-    default:
-        console.log(RandVal); 
-};
+
+const Value = prompt("Enter number from 1 to 100");
+
+function isMult(a, b) {
+    return a % b === 0;
+  }
+
+
+if (Boolean(+Value)) {
+
+// while
+let a = 0;
+ while (a <= +Value) {
+    
+    switch (true) {
+     //   case !!!(a % 15):
+          case isMult(a, 15):
+            console.log("ЛолКек");
+        break;
+      //  case !!!(a % 3):
+      case isMult(a, 3):
+            console.log("Лол");
+        break;
+      //  case !!!(a % 5):
+      case isMult(a, 5):
+            console.log("Кек");
+        break;
+        default:
+            console.log(a); 
+    };
+   a++;
+ }
+
+}  // (Boolean(+Value)) 
+else { //(Boolean(+Value)) 
+       console.log("Щось пішло не так");
+     } // else (Boolean(+Value)) 
+
+
+
 
 /*
 Напишіть код який бере значення зі змінної (`value`) і виводить у консоль всі парні числа, що менші за задане.
@@ -30,8 +57,8 @@ switch (true) {
 2. Код має перевіряти, чи значення у змінній є числом. Якщо воно не є числом,  
 в консоль має вивестись повідомлення про помилку, наприклад `'Таке чуство шо Бог десь наказує нас за шось'`.
 */
-
-const Value = prompt("Enter number");
+console.log("--------------- 2 ------------------");
+//const Value = prompt("Enter number");
 const isEven = n => !!!(n % 2); // Перевірка парності
 
 
