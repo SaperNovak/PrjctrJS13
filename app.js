c3a1aeda665b8586e02d8f468b2875f09230da5d
SaperNovak <98357434+SaperNovak@users.noreply.github.com>
2024-09-02 15:30:51 +0300

    Initial commit for HW7

--- app.js
+++ app.js
@@ -1,89 +1,100 @@
 "use strict";
 
-/*
-Напишіть код який виводить в консоль значення від 1 до 100, 
-але замість чисел кратних 3 має виводитись `Лол`, замість значень кратних 5 - `Кек`, 
-а замість значень кратних і 3, і 5 - `ЛолКек
-*/
-//const RandVal = Math.trunc(Math.random() * 100);
-//console.log(RandVal); 
+// 1. Напишіть функцію detonatorTimer(delay) використовуючи setInterval  
+//detonatorTimer(7);
+// 3
+// 2
+// 1
+// BOOM!
 
-
-const Value = prompt("Enter number from 1 to 100");
-
-function isMult(a, b) {
-    return a % b === 0;
-  }
-
-
-if (Boolean(+Value)) {
-
-// while
-let a = 0;
- while (a <= +Value) {
+function detonatorTimer(delay) {
     
-    switch (true) {
-     //   case !!!(a % 15):
-          case isMult(a, 15):
-            console.log("ЛолКек");
-        break;
-      //  case !!!(a % 3):
-      case isMult(a, 3):
-            console.log("Лол");
-        break;
-      //  case !!!(a % 5):
-      case isMult(a, 5):
-            console.log("Кек");
-        break;
-        default:
-            console.log(a); 
-    };
-   a++;
- }
-
-}  // (Boolean(+Value)) 
-else { //(Boolean(+Value)) 
-       console.log("Щось пішло не так");
-     } // else (Boolean(+Value)) 
-
-
-
-
-/*
-Напишіть код який бере значення зі змінної (`value`) і виводить у консоль всі парні числа, що менші за задане.
-Наприклад, якщо у змінній число 10, програма має вивести наступне: 2, 4, 6, 8. 
-Додаткові вимоги:
-1. Реалізуйте рішення 2 способами: з використанням циклу `for` та `while`
-2. Код має перевіряти, чи значення у змінній є числом. Якщо воно не є числом,  
-в консоль має вивестись повідомлення про помилку, наприклад `'Таке чуство шо Бог десь наказує нас за шось'`.
-*/
-console.log("--------------- 2 ------------------");
-//const Value = prompt("Enter number");
-const isEven = n => !!!(n % 2); // Перевірка парності
-
-
-if (Boolean(+Value)) {
-// For
-     for (let i = 1; i <= +Value; i++) {
-          if (isEven(i) )  {
-              console.log(i);
-          }
-         // else {
-         // continue;  
-         // }
-     } //for
-// while
-let k = 1;
- while (k <= +Value) {
+    let timer = setInterval(() => {
     
-    if (isEven(k) )  {
-        console.log(k);
+    if ( delay > 0){
+    console.log(delay);    
+    delay--;
+    } else {
+    console.log('BOOM!');
+    clearInterval(timer);
     }
-   k++;
- }
+    }, 1000);
+        
+}
 
-}  // (Boolean(+Value)) 
-else { //(Boolean(+Value)) 
-       console.log("Яка прикра несподіванка спіткала наших на самому старті :(");
-     } // else (Boolean(+Value)) 
+//2. Напишіть функцію detonatorTimer(delay) використовуючи вкладений setTimeout  
+//Вона виводить в консоль число кожну секунду, починаючи з delay (ціле число) і в кінці замість 0 виведе 'BOOM!'
+
+//detonatorTimer2(3); Викличу цю функцію як зповільнену задачею 5 для послідовного виводу результатів першого та другого зворотних відліків
+// 3
+// 2
+// 1
+// BOOM!
+
+function detonatorTimer2(delay) {
+
+    if (delay > 0) {
+        console.log(delay);
+        setTimeout(() => detonatorTimer2(delay - 1), 1000);
+      } else {
+         console.log("BOOM!");
+      }
+}
+
+// 5. Напишіть функцію-декоратор яка вповільнює виконання довільної функції на вказану кількість секунд.
+
+function slower(func, seconds) {
+    return function (...args) {
+      console.log(`Chill out, you will get your result in ${seconds} seconds`);
+      // задамо таймаут у кс-ть секунд * 1000мс для виклику ф-ції "func"
+      setTimeout(() => {
+        func(...args);
+      }, seconds * 1000);
+    };
+  }
+  
+  // 
+ 
+  
+  // Обгортаємо функцію 'someFunction' в декоратор і задаємо значення вповільнення
+  const slowedSomeFunction = slower(detonatorTimer, 5); // виведу другий зворотний відлік з затримкою 8 секунд
+  
+  slowedSomeFunction(5,5); // delay = 5 for detonatorTimer2
+
+  const slowedSomeFunction2 = slower(detonatorTimer2, 15); // виведу другий зворотний відлік з затримкою 8 секунд
+  
+  slowedSomeFunction2(3,15); // delay = 3 for detonatorTimer2
+
+
+// 3. Напишіть об'єкт в якому опишіть свої довільні властивості та довільні методи що ці властивості виводять. 
+
+let me = {
+	name: 'Olga',
+	gender: 'female',
+	job: 'SAP consultant',
+    driverLicense: ['A','B'],
+	defaultLang: 'ukrainian',
+	foreinLang: 'english',
+    defaultLangLavel: 'native speaker',
+    foreinLangLevel: 'upper-intermediate',
+    introduce() {
+		console.log(`My name is ${this.name} and I work as ${this.job}`);
+	},
+	
+	describeMySkils(){
+		console.log(`I speak ${this.defaultLang} on on level ${this.foreinLangLevel}, but now I'm also speak ${this.foreinLang} on level ${this.foreinLangLevel}`);
+	}
+}
+
+//me.introduce();
+//me.describeMySkils();
+
+// 4. А тепер зробіть всі свої методи з попередньої задачі прив'язаними до контексту свого об'єкту
+
+// Аби вони були захищені від перезапису об'єкту і їх можна було викликати в таймері:
+let securedSelfIntroduce = me.introduce.bind(me); 
+let securedSelfeDescribe = me.describeMySkils(me);
+
+setTimeout(securedSelfIntroduce, 1000); // виведе коректний результат
+setTimeout(securedSelfeDescribe, 1000); // виведе коректний результат
 
